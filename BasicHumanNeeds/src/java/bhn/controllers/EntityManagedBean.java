705d1f307225bae5c927f59d15738fad4f167216
Alison Perez <adp05@live.com>
2012-06-02 12:05:09 -0400

    packages

---
+++ BasicHumanNeeds/src/java/bhn/controllers/EntityManagedBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bhn.controllers;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Alison David
+ */
+@ManagedBean
+@RequestScoped
+public class EntityManagedBean {
+
+    /**
+     * Creates a new instance of EntityManagedBean
+     */
+    public EntityManagedBean() {
+    }
+}
